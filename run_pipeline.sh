95d168e46ad5d7125e71679561903151966613ba
Adil Hannaoui Anaaoui <154902011+AdilHannaoui@users.noreply.github.com>
2026-02-09 12:07:10 +0100

    Update run_pipeline.sh

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -3,47 +3,51 @@
 set -e
 set -o pipefail
 
+mkdir -p logs
+
 SECONDS=0
 echo "=== Pipeline started successfully ===)"
 
-# 1. Environment Activation
-echo "[1/10] Activating Conda environment..."
+echo "[1/11] Activating Conda environment..."
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate ripseq-rpb4
 
-# 2. Configuration loading
-echo "[2/10] Loading R config..."
+echo "[2/11] Loading configs..."
 CONFIG_R="R/config.R"
-echo "[2/7] Loading bash config..."
 CONFIG_bash="bash/config.sh"
 
+echo "[3/11] Running FastQC..."
+bash bash/01-fastqc.sh $CONFIG_bash > logs/fastqc.log 2>&1
 
-# 3. QC
-echo "[3/10] Running FastQC..."
-bash bash/01-fastqc.sh $CONFIG_bash
+echo "[4/11] Running Trimmomatic..."
+bash bash/02-trimming.sh $CONFIG_bash > logs/trimming.log 2>&1
 
-# 4. Trimming
-echo "[4/10] Running Trimmomatic..."
-bash bash/02-trimming.sh $CONFIG_bash
+echo "[5/11] Running Bowtie2..."
+bash bash/03-alignment_bowtie2.sh $CONFIG_bash > logs/alignment.log 2>&1
 
-# 5. Alignment
-echo "[5/10] Running Bowtie2..."
-bash bash/03-alignment_bowtie2.sh $CONFIG_bash
+echo "[6/11] Running FeatureCounts..."
+bash bash/04-featurecounts.sh $CONFIG_bash > logs/featurecounts.log 2>&1
 
-# 6. Peak Calling
-echo "[6/10] Running macs2..."
-bash bash/04-featurecounts.sh $CONFIG_bash
+echo "[7/11] Running MACS3..."
+bash bash/05-peak_calling_exploratory.sh $CONFIG_bash > logs/macs3.log 2>&1
 
-# 7. Peak Intersection
-echo "[7/10] Making Peak Intersection..."
-bash bash/05-peak_calling_exploratory.sh $CONFIG_bash
+echo "[8/11] Running Peak Intersection..."
+bash bash/06-peak_intersection_exploratory.sh $CONFIG_bash > logs/intersection.log 2>&1
 
-# 8. R Analysis
-echo "[8/10] Running DESeq2 and downstream analysis..."
-Rscript R/01-load_counts.R $CONFIG_R
-Rscript R/02-deseq2_analysis.R $CONFIG_R
-Rscript R/02.1-deseq2_peaks_analysis.R $CONFIG_R
+echo "[9/11] Running Window Analysis..."
+bash bash/07-window_analysis_exploratory.sh $CONFIG_bash > logs/windows.log 2>&1
 
+echo "[10/11] Running TSS metagene Analysis..."
+bash bash/08-tss_metagene.sh $CONFIG_bash > logs/tss_metagene.log 2>&1
+
+echo "[11/11] Running R analysis..."
+Rscript R/01-load_counts.R $CONFIG_R > logs/load_counts.log 2>&1
+Rscript R/02-deseq2_analysis.R $CONFIG_R > logs/deseq2.log 2>&1
+Rscript R/02.1-deseq2_peaks_analysis.R $CONFIG_R > logs/deseq2_peaks.log 2>&1
+Rscript R/03-integrate_genes.R $CONFIG_R > logs/integration.log 2>&1
+Rscript R/04-enrichment_analysis.R $CONFIG_R > logs/enrichment.log 2>&1
+Rscript R/05-visualizations.R $CONFIG_R > logs/visualizations.log 2>&1
 
 duration=$SECONDS
 echo "Total execution time: $((duration / 60)) minutes $((duration % 60)) seconds"
+
