5af6b76619d94df84e8ac5ae1d40b9d1a1fe982e
Adil Hannaoui Anaaoui <154902011+AdilHannaoui@users.noreply.github.com>
2026-02-26 13:30:22 +0100

    Update config.R

--- R/config.R
+++ R/config.R
@@ -3,27 +3,6 @@
 # Author: Adil Hannaoui Anaaoui
 # ==========================
 
-# --------------------------
-# Project structure
-# --------------------------
-PROJECT_ROOT <- normalizePath(file.path(dirname(sys.frame(1)$ofile), ".."))
-
-DATA_DIR <- file.path(PROJECT_ROOT, "data")
-GTF_DIR <- file.path(PROJECT_ROOT, "Bowtie2")
-OUTPUT_DIR <- file.path(PROJECT_ROOT, "output")
-DESEQ2_FC_DIR <- file.path(OUTPUT_DIR, "DESeq2 FeatureCounts")
-BAM_DIR <- file.path(OUTPUT_DIR, "bowtie2")
-DESEQ2_MACS3_DIR <- file.path(OUTPUT_DIR, "DESeq2 Macs3")
-WINDOWS_DIR <- file.path(OUTPUT_DIR, "Windows Analysis")
-
-
-GTF <- file.path(GTF_DIR, "cerevisiae/Saccharomyces_cerevisiae.R64-1-1.112.gtf")
-COUNTS_MATRIX_PATH_FC <- file.path(DESEQ2_FC_DIR, "counts_matrix.rds")
-SAMPLE_METADATA_PATH_FC <- file.path(DESEQ2_FC_DIR, "colData.rds")
-DDS_FC <- file.path(DESEQ2_FC_DIR, "dds.rds")
-FEATURECOUNTS_DIR <- file.path(OUTPUT_DIR, "featurecounts")
-PLOTS_DIR <- file.path(OUTPUT_DIR, "Plots")
-
 # ============================================================
 # DATASET SELECTION
 # ============================================================
@@ -47,7 +26,31 @@ if (DATASET == "test") {
     rep("M12_IP", 3),
     rep("WT_IN", 3),
     rep("WT_IP", 3)
+    )
 }
+
+# --------------------------
+# Project structure
+# --------------------------
+PROJECT_ROOT <- normalizePath(file.path(dirname(sys.frame(1)$ofile), ".."))
+
+DATA_DIR <- file.path(PROJECT_ROOT, "data")
+GTF_DIR <- file.path(PROJECT_ROOT, "Bowtie2")
+OUTPUT_DIR <- file.path(PROJECT_ROOT, "output")
+DESEQ2_FC_DIR <- file.path(OUTPUT_DIR, "DESeq2 FeatureCounts")
+BAM_DIR <- file.path(OUTPUT_DIR, "bowtie2")
+MACS3_DIR <- file.path(OUTPUT_DIR, "macs3")
+DESEQ2_MACS3_DIR <- file.path(OUTPUT_DIR, "DESeq2 Macs3")
+WINDOWS_DIR <- file.path(OUTPUT_DIR, "Windows Analysis")
+
+
+GTF <- file.path(GTF_DIR, "cerevisiae/Saccharomyces_cerevisiae.R64-1-1.112.gtf")
+COUNTS_MATRIX_PATH_FC <- file.path(DESEQ2_FC_DIR, "counts_matrix.rds")
+SAMPLE_METADATA_PATH_FC <- file.path(DESEQ2_FC_DIR, "colData.rds")
+DDS_FC <- file.path(DESEQ2_FC_DIR, "dds.rds")
+FEATURECOUNTS_DIR <- file.path(OUTPUT_DIR, "featurecounts")
+PLOTS_DIR <- file.path(OUTPUT_DIR, "Plots")
+
 
 # --------------------------
 # DESeq2 parameters
@@ -68,4 +71,5 @@ QVAL_CUTOFF <- 0.05
 # --------------------------
 ORG_DB <- "org.Sc.sgd.db"
 GENE_ID_TYPE <- "ORF" 
+
 
